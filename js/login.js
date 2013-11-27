119cc218ae7cf349415be397142d0d5f9b1c9ddb
Anthony <anthony.vernagallo@gmail.com>
2013-11-27 11:22:05 +0100

    La page de login respecte le scenario obligatoire

--- js/login.js
+++ js/login.js
@@ -7,11 +7,35 @@ function init() {
 								 for(p in data.gamesList) {
 									 var li = document.createElement('li'); 
 									 ul.appendChild( li );
-									 li.appendChild( document.createTextNode( data.gamesList[p] ) );
+									 var a = document.createElement('a');
+									 a.setAttribute('href', '#');
+									 a.addEventListener('click', function() {
+										document.getElementById('idGame').value = data.gamesList[p];
+										document.getElementById('nouvellePartie').submit();
+										return false;
+									 });
+									 a.appendChild( document.createTextNode( data.gamesList[p] ) );
+									 li.appendChild(a);
 									}
 								}
 			 );
 	socket.emit('loginPage');
+	
+	document.getElementById('nouvellePartie').addEventListener('submit', function(e) {
+		if (document.getElementById('login').value != '' && document.getElementById('idGame').value == '') {
+			console.log('login=', document.getElementById('login').value);
+			document.getElementById('champLogin').style.display = 'none';
+			document.getElementById('champIdGame').style.display = 'block';
+			document.getElementById('idGame').focus();
+			e.preventDefault();
+			return false;
+		} else if (document.getElementById('idGame').value != '') {
+			// on envoie !
+		} else {
+			e.preventDefault();
+			return false;
+		}
+	});
 }
 
 
