3d8fe2262a1108381e70a39c4a03bae14a976db0
disavinr <remi.disavino@gmail.com>
2013-12-04 10:30:55 +0100

    merge

--- js/login.js
+++ js/login.js
@@ -2,23 +2,31 @@ function init() {
 	// Connect to the SocketIO server to retrieve ongoing games.
 	socket = io.connect();
 	socket.on('gamesList', function(data) {
-								 var ul = document.getElementById('lesParties');
-								 ul.innerHTML='';
-								 for(p in data.gamesList) {
-									 var li = document.createElement('li'); 
-									 ul.appendChild( li );
-									 var a = document.createElement('a');
-									 a.setAttribute('href', '#');
-									 a.addEventListener('click', function() {
-										document.getElementById('idGame').value = data.gamesList[p];
-										document.getElementById('nouvellePartie').submit();
-										return false;
-									 });
-									 a.appendChild( document.createTextNode( data.gamesList[p] ) );
-									 li.appendChild(a);
+								var ul = document.getElementById('lesParties');
+								ul.innerHTML='';
+								if(data.gamesList.length > 0)
+								{
+									for(p in data.gamesList) {
+										var li = document.createElement('li'); 
+										ul.appendChild( li );
+										var a = document.createElement('a');
+										a.setAttribute('href', '#');
+										a.addEventListener('click', function() {
+											document.getElementById('idGame').value = data.gamesList[p];
+											document.getElementById('nouvellePartie').submit();
+											return false;
+										});
+										a.appendChild( document.createTextNode( data.gamesList[p] ) );
+										li.appendChild(a);
 									}
+								} else {
+									var li = document.createElement('li'); 
+									ul.appendChild(li);
+									li.appendChild(document.createTextNode('Aucune partie disponible'));
+									li.className = 'empty';
 								}
-			 );
+							}
+			);
 	socket.emit('loginPage');
 	
 	document.getElementById('nouvellePartie').addEventListener('submit', function(e) {
